e83a4356b9e14a72f6ca1aa375d1ad26f56323d1
Pontus Abrahamsson <pontus@lostisland.co>
2024-12-30 14:00:08 +0100

    Add avatar

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -1,10 +1,11 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: text("id").primaryKey(),
   email: text("email").notNull().unique(),
   name: text("name").notNull(),
+  avatar: text("avatar"),
   provider: text("provider", { enum: ["github", "google"] }).notNull(),
   createdAt: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: text("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
