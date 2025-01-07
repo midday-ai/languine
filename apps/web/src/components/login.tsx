eaf1b5ab6e7567af5f2153c8246e7039ff2dfc9c
Pontus Abrahamsson <pontus@lostisland.co>
2025-01-07 23:24:51 +0100

    Update login.tsx

--- apps/web/src/components/login.tsx
+++ apps/web/src/components/login.tsx
@@ -7,15 +7,15 @@ export default async function Login() {
   const t = await getI18n();
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 px-4">
       <h2 className="text-2xl font-normal">{t("login.title")}</h2>
       <p className="text-secondary">{t("login.description")}</p>
-      <div className="flex flex-row gap-6 w-full mt-4">
+      <div className="flex flex-col sm:flex-row gap-4 w-full mt-4">
         <GithubSignIn />
         <GoogleSignIn />
       </div>
 
-      <div className="absolute bottom-12 max-w-xl">
+      <div className="absolute bottom-12 max-w-xl px-4">
         <div className="bg-dotted mb-6 h-9 w-full" />
         <p className="text-secondary text-sm">
           {t("login.terms.text")}{" "}
