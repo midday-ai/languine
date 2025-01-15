4c214a4015f4c184223349020b9112a60f7cbfb6
Pontus Abrahamsson <pontus@lostisland.co>
2025-01-15 17:38:41 +0100

    wip

--- apps/web/src/app/api/auth/cli/[token]/route.ts
+++ apps/web/src/app/api/auth/cli/[token]/route.ts
@@ -1,19 +1,20 @@
 import { getSession } from "@/lib/session";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { token: string } },
+  { params }: { params: Promise<{ token: string }> },
 ) {
-  // const cookieStore = await cookies();
+  const { token } = await params;
+
   const session = await getSession();
 
   if (!session?.data) {
     const response = NextResponse.redirect(new URL("/login", request.url));
-    if (params.token) {
-      response.cookies.set("cli-token", params.token, {
-        maxAge: 5 * 60, // 5 minutes in seconds
+
+    if (token) {
+      response.cookies.set("cli-token", token, {
+        maxAge: 5 * 60,
       });
     }
 
