62632ec4f6bf05badef549a5f2ab49ad4e96a48d
Pontus Abrahamsson <pontus@lostisland.co>
2025-01-28 10:46:40 +0100

    Mobile menu

--- apps/web/src/components/mobile-menu.tsx
+++ apps/web/src/components/mobile-menu.tsx
@@ -23,27 +23,6 @@ export function MobileMenu() {
   const params = useParams();
   const t = useTranslations("menu");
 
-  const navigation = [
-    {
-      icon: MdOutlineStackedBarChart,
-      path: "/",
-      isActive: pathname.endsWith(`/${params.organization}/${params.project}`),
-      label: t("dashboard"),
-    },
-    {
-      icon: MdGraphicEq,
-      path: "/tuning",
-      isActive: pathname.endsWith("/tuning"),
-      label: t("tuning"),
-    },
-    {
-      icon: MdOutlineSettings,
-      path: "/settings",
-      isActive: pathname.endsWith("/settings"),
-      label: t("settings"),
-    },
-  ];
-
   const links = [
     { href: "/pricing", label: t("pricing") },
     { href: "https://git.new/languine", label: t("docs") },
@@ -83,33 +62,13 @@ export function MobileMenu() {
             className="fixed inset-0 z-50 bg-background bg-noise w-full bottom-0 h-screen left-0 right-0 top-[50px]"
           >
             <div className="flex flex-col h-full">
-              {navigation.map((item, index) => (
-                <motion.div
-                  key={item.path}
-                  initial={{ opacity: 0, x: -20 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ delay: index * 0.02, duration: 0.1 }}
-                >
-                  <Link
-                    href={item.path}
-                    className={cn(
-                      "block py-5 text-secondary",
-                      pathname?.endsWith(item.path) && "text-primary",
-                    )}
-                    onClick={() => setIsOpen(false)}
-                  >
-                    {item.label}
-                  </Link>
-                </motion.div>
-              ))}
-
               {links.map((link, index) => (
                 <motion.div
                   key={link.href}
                   initial={{ opacity: 0, x: -20 }}
                   animate={{ opacity: 1, x: 0 }}
                   transition={{
-                    delay: navigation.length * 0.02 + index * 0.02,
+                    delay: links.length * 0.02 + index * 0.02,
                     duration: 0.1,
                   }}
                 >
@@ -133,7 +92,7 @@ export function MobileMenu() {
                 initial={{ opacity: 0, x: -20 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{
-                  delay: navigation.length * 0.02 + links.length * 0.02 + 0.05,
+                  delay: links.length * 0.02 + 0.05,
                   duration: 0.1,
                 }}
               >
